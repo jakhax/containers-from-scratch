5db6cde98c688ae49fcce3881dfba6d6a907c50a
Jack Ogina <jackogina60@gmail.com>
2019-11-14 06:38:44 +0300

    addded groups & namespaces

--- README.md
+++ README.md
@@ -5,12 +5,34 @@ I came across the concept of containers.
 `It isolates an application and its dependencies into a self-contained unit that can run anywhere.`
 - In my opinion, containers are really trending/hyped technology that is poorly understood, so I think it would be nice to create one by yourself to understand how it works.
 
+## Running this example 
+- You'll need to do it from a linux machine.
+- First download the ubuntu file system, you can get it from here, [ubuntu_fs.zip](https://drive.google.com/file/d/1Qunrj-3_oXhPQCz4aqaYffiGBY9DdRR9/view?usp=sharing),  unzip it at the root of this project.
+
+```bash
+# needs root privilege for creating cgroup
+sudo su
+go run main.go run /bin/bash
+``` 
+
+### How it works
+
+- Parent process fork itself with `CLONE_NEWUTS`, `CLONE_NEWPID`, `CLONE_NEWNS` flags with isolated hostname, processes and mounts
+- The forked process will create `cgroup` to limit memory usage of itself and any child process it creates
+- Mount `./ubuntu_fs` directory as root filesystem using `chroot` to limit access to host machine's filesystem
+- Mount `/mytemp` directory as tmpfs. Any change made to this directory will not be visible from host.
+- Mount proc (where `CLONE_NEWPID` namespace was already set) so that container can run `ps` and see only the processes running inside it.
+- Execute the supplied argument `/bin/bash` inside the isolated environment
+
+
 ## Resources
 How this work is already well explained by the following resources/tutorials
 - [Containers From Scratch by Liz Rice (video)](https://www.youtube.com/watch?v=8fi7uSYlOdc)
 - [Build your own container from Scratch](https://www.infoq.com/articles/build-a-container-golang/)
 - [Docker From Scratch with python workshop](https://github.com/Fewbytes/rubber-docker)
 - [A Beginner-Friendly Introduction to Containers, VMs and Docker](https://www.freecodecamp.org/news/a-beginner-friendly-introduction-to-containers-vms-and-docker-79a9e3e119b/)
+- [Namespaces in Go Series](https://medium.com/@teddyking/linux-namespaces-850489d3ccf)
+- [Understand Container](https://pierrchen.blogspot.com/2018/08/understand-container-index.html)
 
 ## Security of Containers
  **Containers are not contained**
--- main.go
+++ main.go
@@ -1,36 +1,77 @@
-package main;
+package main
 
 import (
-	"fmt"
-	"os"
 	"log"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
-);
+	"path/filepath"
+	"strconv"
+	"syscall"
+)
 
-// go run main.go run command args...
-func main()  {
-	if(len(os.Args) < 3){
-		usage := "Usage: go run main.go cmd args...";
-		log.Fatal(usage);
-	}
-	switch(os.Args[1]){
+// go run main.go run <cmd> <args>
+func main() {
+	switch os.Args[1] {
 	case "run":
-		run();
-		break;
+		run()
+	case "child":
+		child()
 	default:
-		log.Fatalf("Unknown command %s \n",os.Args[1])
+		panic("help")
 	}
 }
 
+func run() {
+	fmt.Printf("Running %v \n", os.Args[2:])
 
-func run()  {
-	fmt.Printf(" Running command: %s %v\n",os.Args[2],os.Args[3:]);
-	cmd := 	exec.Command(os.Args[2],os.Args[3:]...);
-	cmd.Stdin = os.Stdin;
-	cmd.Stdout = os.Stdout;
-	cmd.Stderr  = os.Stderr;
-	err := cmd.Run();
-	if(err != nil){
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+
+	HandleError(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v \n", os.Args[2:])
+
+	cg()
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	HandleError(syscall.Sethostname([]byte("container")))
+	HandleError(syscall.Chroot("/home/octojob/go/src/github.com/jakhax/containers-from-scratch/ubuntu_fs/"))
+	HandleError(os.Chdir("/"))
+	HandleError(syscall.Mount("proc", "proc", "proc", 0, ""))
+	HandleError(syscall.Mount("thing", "mytemp", "tmpfs", 0, ""))
+
+	HandleError(cmd.Run())
+
+	HandleError(syscall.Unmount("proc", 0))
+	HandleError(syscall.Unmount("thing", 0))
+}
+
+func cg() {
+	cgroups := "/sys/fs/cgroup/"
+	pids := filepath.Join(cgroups, "pids")
+	os.Mkdir(filepath.Join(pids, "liz"), 0755)
+	HandleError(ioutil.WriteFile(filepath.Join(pids, "liz/pids.max"), []byte("20"), 0700))
+	// Removes the new cgroup in place after the container exits
+	HandleError(ioutil.WriteFile(filepath.Join(pids, "liz/notify_on_release"), []byte("1"), 0700))
+	HandleError(ioutil.WriteFile(filepath.Join(pids, "liz/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+}
+
+func HandleError(err error) {
+	if err != nil {
 		log.Fatal(err);
 	}
 }
